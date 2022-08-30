3c6415f1ae42d06157c4495a2dfed71af164653a
MetalKaiser <metalkaiserpolanco@gmail.com>
2022-08-30 14:55:57 -0400

    Paasport auth functions added

--- app.js
+++ app.js
@@ -1,104 +1,184 @@
 const express = require('express');
-const bp = require('body-parser');
-const crypto = require('crypto');
-const { promisify } = require('util');
 
-var users = require('./src/routers/users');                   //router for user characteristics
-var { closeServer, main } = require('./src/config/database'); //database functions
-var user = require('./app/models/user');                       //class model for new users
-const { count } = require('console');
+require('dotenv').config();
+const bp = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const bcrypt = require('bcrypt');
+const saltOrRounds = 12;
+
+const session = require('express-session');
 
 const app = express();
-
-//Using body-parser for req.body
-app.use(bp.json())
-app.use(bp.urlencoded({ extended: true }))
-
-//Declaring the port for the server
 const port = process.env.PORT;
 
-/*
-  Route for registering new users
-*/
-app.post('/register', function (req, res) {
-  var formdata = req.body;
+mongoose.connect(process.env.DBURI);
 
-  //Connecting to the database
-  main()
-    .then(response => resultado(response))
-    .catch(console.error);
+const Schema = mongoose.Schema;
+const user = new Schema({
+  id: Number,
+  email: String,
+  password: String,
+  details: {
+    name: String,
+    lastname: String,
+    birthdate: Date,
+    gender: String
+  },
+  history: {
+    date: Date,
+    height: String,
+    weight: String,
+    pfat: Number
+  },
+  profile: {
+    theme: String,
+    lang: String
+  }
+});
 
-  //handling the connection to the database
-  async function resultado(response) {
-    const collection = response.collection('personas');   //mongodb collection
-    var findResult = await collection.countDocuments();
-    var id = ++findResult;
-    var salt = crypto.randomBytes(16).toString("hex");    //generating the salt
-    
-    crypto.scrypt(formdata.password,salt,64,(err,derivedKey) => {
-      if (err) {
-        console.log(err);
-        reject(err);
+const userDetails = mongoose.model('UserInfo',user,'personas');
+user.clearIndexes();
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+
+user.plugin(passportLocalMongoose);
+
+app.use(bp.urlencoded({ extended: false }));
+app.use(bp.json());
+
+passport.use(new LocalStrategy(
+  { 
+    usernameField: 'username',
+    passwordField: 'password'
+  },
+  async (username,password,done) => {
+    userDetails.findOne({email: username}, async (err, usuario) => {
+      if (err) { 
+        console.log("error");
+        return done(err); 
       }
-      var password = salt + ":" + derivedKey.toString('hex');   //generating the hashed password
-      var newuser = new user(id,formdata.email,password,formdata.name,formdata.lastname,formdata.birthdate,formdata.gender);
-      try {
-        dbInsert(collection, newuser);
-      } catch (error) {
-        res.send(error);
+      if (!usuario) { 
+        console.log("Not found");
+        return done(null, false);
       }
+      var hash = usuario.password;
+      var boolPass = await bcrypt.compare(password, hash);
+      if (!boolPass) {
+        console.log("Contraseña incorrecta");
+        return done(null, false); 
+      }
+      console.log("Éxito");
+      return done(null, usuario);
     });
   }
+));
 
-  //handling the insert on the collection
-  async function dbInsert(collection, newuser) {
-    const insertResult = await collection.insertOne(newuser);
-    res.send(insertResult);
-    closeServer();
-  }
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+  done(
+    null,
+    (id) => userDetails.findOne({id: id})
+  );
 });
 
-/*
-  Route for logging in the app
-*/
-app.post('/login', function (req, res) {
-  var formdata = req.body;
-  const scrypt = promisify(crypto.scrypt);
-  //Connecting to the database
-  main()
-    .then(response => resultado(response))
-    .catch(console.error);
+app.use(passport.initialize());
+app.use(passport.session());
 
-  //handling the connection to the database
-  async function resultado(response) {
-    const collection = response.collection('personas');
-    var findResult = await collection.findOne({email : formdata.email});
-    closeServer();
-    
-    if (findResult == null) {
-      res.send("No encontrado");
-    } else {
-      verify(formdata.password, findResult.password);
+const checkAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) { return next() }
+  res.redirect("/login")
+}
+
+const checkUnauthenticated = (req, res, next) => {
+  if (req.isUnauthenticated()) { return next() }
+  res.status(603).redirect('/');
+}
+
+const newuser = async (req, n) => {
+  var hash = await bcrypt.hash(req.body.password,saltOrRounds);
+  var userinsert = {
+    'id': n+1,
+    'email': req.body.username,
+    'password': hash,
+    'details': {
+      'name': req.body.name,
+      'lastname': req.body.lastname,
+      'birthdate': new Date(req.body.birthdate),
+      'gender': req.body.gender
+    },
+    'history': {
+      'date': null,
+      'height': "",
+      'weight': "",
+      'pfat': null
+    },
+    'profile': {
+      'theme': 'light',
+      'lang': 'es'
     }
   }
-
-  //Checking if password is correct
-  async function verify(password, hash) {
-    const [salt, key] = hash.split(":");        //Extract the salt
-    const keyBuffer = Buffer.from(key, 'hex');
-    const derivedKey = await scrypt(password, salt, 64);
-    var boolPass = crypto.timingSafeEqual(keyBuffer, derivedKey);   //Comparing the password
-    
-    if(boolPass) {
-      res.send("Bienvenido!");
+  if (req.body.username != "" && req.body.password != "" && req.body.name != "" && req.body.lastname != "" && req.body.birthdate != "" && req.body.gender != "") {
+    var validateEmail = await userDetails.findOne({email:req.body.username});
+    if (validateEmail != null) {
+      return "userExists";
     }else {
-      res.send("Credenciales inválidas");
+      try {
+        var dbRes = await userDetails.create(userinsert);
+        if (n == 0) {
+          
+        }
+        return dbRes;
+      } catch (error) {
+        return error;
+      }
     }
+  }else {
+    return "emptyFields";
   }
+}
+
+app.get('/', checkAuthenticated, (req, res) => {
+  res.send('Vista de bienvenida');
 });
 
-app.use('/users', users);
+app.get('/login', function(req, res) {
+  res.send('Vista de Login');
+});
+
+app.post('/login', passport.authenticate('local', { 
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureMessage: true
+}),
+  (req, res) => {
+    res.redirect('/');
+});
+
+app.post('/register', checkUnauthenticated, async (req, res) => {
+  var n = await userDetails.count();
+  var result = await newuser(req, n);
+  console.log(result);
+  res.send('Vista de registro');
+});
+
+app.delete("/logout", (req,res) => {
+  req.logOut(
+    (err) => {
+      if (err) { return next(err); }
+    });
+  console.log(`-------> User Logged out`);
+  res.redirect("/login");
+});
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
+  console.log(`Server listening on port ${port}`);
 });

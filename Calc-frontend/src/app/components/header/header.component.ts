2f17dcacf91ee7d8a4cb3b979384660e9e8654da
MetalKaiser <metalkaiserpolanco@gmail.com>
2022-10-13 11:57:16 -0400

    Logout agregado

--- Calc-frontend/src/app/components/header/header.component.ts
+++ Calc-frontend/src/app/components/header/header.component.ts
@@ -16,19 +16,20 @@ export class HeaderComponent implements OnInit {
   }
 
   logout(){
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        console.log("Cookie borrada");
+    }
+
     this.authServ.logout().subscribe(
       response => {
-        console.log(Object.values(response)[0]);
         if (Object.values(response)[0]) {
-          var cookies = document.cookie.split(";");
-
-          for (var i = 0; i < cookies.length; i++) {
-              var cookie = cookies[i];
-              var eqPos = cookie.indexOf("=");
-              var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-              document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-              console.log("Cookie borrada");
-          }
+          window.location.reload(); 
         }
       }
     )
